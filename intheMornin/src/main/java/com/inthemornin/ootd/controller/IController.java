121f957c54730953b6f50cacb632f6cb681651ad
cksgh <cksgh@DESKTOP-HTCTA77>
2019-11-12 13:54:18 +0900

    tt

--- intheMornin/src/main/java/com/inthemornin/ootd/controller/IController.java
+++ intheMornin/src/main/java/com/inthemornin/ootd/controller/IController.java
@@ -1,5 +1,20 @@
 package com.inthemornin.ootd.controller;
 
-public interface IController {
+import java.util.List;
+import java.util.Map;
 
-}
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+
